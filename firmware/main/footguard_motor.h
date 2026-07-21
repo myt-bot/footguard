ec4d1e852d0149bf0d8fdf345b7c00c82b98c284
myt <yanfeiquan@126.com>
2026-07-21 18:45:01 +0800

    feat: 添加马达震动模式执行函数

--- firmware/main/footguard_motor.h
+++ firmware/main/footguard_motor.h
@@ -3,6 +3,10 @@
 #include <stdbool.h>
 
 #include "esp_err.h"
+#include "footguard_command.h"
 
 esp_err_t footguard_motor_init(void);
 esp_err_t footguard_motor_set(bool enabled);
+esp_err_t footguard_motor_execute_pattern(
+    footguard_command_pattern_t pattern,
+    uint32_t duration_ms);
